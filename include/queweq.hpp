ac97446aa31e898229d7a9be67389d0af36c8e6f
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-04-13 11:40:29 +0300

    Update queweq.hpp

--- include/queweq.hpp
+++ include/queweq.hpp
@@ -2,3 +2,41 @@
 #include <sstream>
 #include <string>
 
+template <typename T>
+class queue {
+private:
+    struct node_t {
+        node_t * next;
+        T value;
+    };
+    node_t * head;
+    node_t * trail;
+public:
+    queue() {
+        head = nullptr; //начало
+        trail = nullptr; //конец
+    }
+    void push(T value) {
+        node_t * node = new node_t;
+        node->value = value;
+        node->next = nullptr;
+        if( head == nullptr) {
+            head = node;
+            trail = node;
+        }
+        else {
+            trail->next = node;
+            trail = node;
+        }
+    }
+    void print() {
+            if(head != nullptr) {
+            while(head->next != nullptr) {
+                std::cout << head->value << "  ";
+                head = head->next;
+            }
+            std::cout << trail->value << "  ";
+        }
+    }
+
+};
