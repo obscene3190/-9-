d7213586a948d17f429e4909457af51078688181
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-04-13 20:29:27 +0300

    Update queue.cpp

--- tests/queue.cpp
+++ tests/queue.cpp
@@ -10,3 +10,20 @@ TEST_CASE("creating")
 	REQUIRE( queue.tail() == nullptr );
 }
 
+TEST_CASE("push pop")
+{
+	queue_t<int> queue;
+	std::string result1{"6  5  4  "};
+	queue.push(6);
+	queue.push(5);
+	queue.push(4);
+	std::ostringstream ostream;
+	queue.print(ostream);
+	REQUIRE( ostream.str() == result );
+	std::string result1{"5  4  "};
+	std::ostringstream ostream1;
+	queue.print(ostream1);
+	REQUIRE( ostream1.str() == result1 );
+}
+
+
