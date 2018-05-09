ad9c2c678c40364fd70dad29a5e51f96f67b5d5b
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-05-09 15:37:29 +0300

    Update graph.hpp

--- include/graph.hpp
+++ include/graph.hpp
@@ -32,26 +32,33 @@ public:
   }
   
   void set_vertices(unsigned int vertices) {
-    size = vertices
+    size = vertices;
   }
   
-  ostream print(std::ostream & stream) {
-    if(roots) {
-      for( int i = 0; i  < size; i++) {
+  void print(std::ostream & stream) {
+    //if(roots) {
+      for( unsigned int i = 0; i  < size; i++) {
         stream<<i+1<<": ";
         vector<T> help = roots[i];
-        for(int j = 0; j<roots[i].size(); j++) {
-          stream<<help[j]<<", ";
+        for(unsigned int j = 0; j<roots[i].size(); j++) {
+          stream<<help[j]<<" ";
         }
         stream<<endl;
       }
     }
-  }
+  //}
   
   ~graph() {};
   
-	void read();
+/*	void read();
 	Graph operator = (Graph & other);
 	void dfs(unsigned index);
-	void input(vector < vector<unsigned> > & in);
+	void input(vector < vector<unsigned> > & in); */
 };
+
+int main() {
+    graph<int> graph1({{ 1 },{ 2,6 },{4,5},{},{}});
+    //graph1.print(cout);
+    graph<int> graph12(graph1);
+    graph12.print(cout);
+}
