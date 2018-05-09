f8e65d1c35e831bf9cbccba08139da31cf98eba1
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-05-09 15:28:26 +0300

    Update graph.hpp

--- include/graph.hpp
+++ include/graph.hpp
@@ -1,1 +1,57 @@
+#include<vector>
+#include<iostream>
+#include<string>
+#include<sstream>
+using namespace std;
 
+template <typename T>
+class graph {
+private:
+  vector<vector<T>> roots;
+  unsigned int size;
+  
+public:
+  graph() {
+    size = 0;
+  }
+  
+  graph(graph<T> &other) {
+    roots = other.returnroots();
+    size = roots.size();
+  }
+  
+  graph(vector<vector<T>> inputroots) {
+    roots = inputroots;
+    size = roots.size();
+  }
+  
+  
+  
+  vector<vector<T>> returnroots() {
+    return roots;
+  }
+  
+  void set_vertices(unsigned int vertices) {
+    size = vertices
+  }
+  
+  ostream print(std::ostream & stream) {
+    if(roots) {
+      for( int i = 0; i  < size; i++) {
+        stream<<i+1<<": ";
+        vector<T> help = roots[i];
+        for(int j = 0; j<roots[i].size(); j++) {
+          stream<<help[j]<<", ";
+        }
+        stream<<endl;
+      }
+    }
+  }
+  
+  ~graph() {};
+  
+	void read();
+	Graph operator = (Graph & other);
+	void dfs(unsigned index);
+	void input(vector < vector<unsigned> > & in);
+};
