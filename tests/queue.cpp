3ae478ca466cf3c3d7ed0dbb67b49b09bbdffcaf
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-05-09 17:27:41 +0300

    Update queue.cpp

--- tests/queue.cpp
+++ tests/queue.cpp
@@ -5,6 +5,26 @@
 
 TEST_CASE("Creating")
 {
-	
+	graph graph1;
+	REQUIRE(graph1.returnsize() == 0);
 }
+
+TEST_CASE("Creating")
+{
+	graph<int> graph1({{1,2},{3},{3},{}});
+	std::ostringstream ostream:
+	graph1.print(otream);
+	REQUIRE(ostream.str() = "0: 1 2 \n"
+				"1: 3 \n"
+				"2: 3 \n"
+				"3: \n";
+}
+		
+TEST_CASE("DFS
+{
+	graph<int> graph1({{1,2},{3},{3},{}});
+	std::ostringstream ostream:
+	graph1.print(otream);
+	REQUIRE(ostream.str() = 
+}	
 
