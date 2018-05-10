235073d1dce235feabdb0905707bdb5331ee4296
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-05-10 17:09:59 +0300

    Update graph.hpp

--- include/graph.hpp
+++ include/graph.hpp
@@ -9,7 +9,7 @@ using namespace std;
 template <typename T>
 class graph {
 private:
-  vector<vector<T>> roots;
+  vector<vector<T>> nodes;
   unsigned int size;
   
 public:
@@ -18,27 +18,27 @@ public:
   }
   
   graph(graph<T> &other) {
-    roots = other.returnroots();
-    size = roots.size();
+    roots = other.returnnodes();
+    size = nodes.size();
   }
   
-  graph(vector<vector<T>> inputroots) {
-    roots = inputroots;
-    for (unsigned i = 0; i < inputroots.size(); i++) {
-	for (const auto& j : inputroots[i]){
-		if (j >= inputroots.size()) {
+  graph(vector<vector<T>> inputnodes) {
+    nodes = inputnodes;
+    for (unsigned i = 0; i < inputnodes.size(); i++) {
+	for (const auto& j : inputnodes[i]){
+		if (j >= inputnodes.size()) {
 			cout<<"Incorrect import";
-			throw length_error("fail");
+			throw length_error("No such root");
 		}
 	}
     }
-    size = roots.size();
+    size = nodes.size();
   }
   
   
   
-  vector<vector<T>> returnroots() {
-    return roots;
+  vector<vector<T>> returnnodes() {
+    return nodes;
   }
   
   unsigned int returnsize() {
@@ -48,28 +48,35 @@ public:
   void print(std::ostream & stream) {
       for( unsigned int i = 0; i  < size; i++) {
         stream<<i<<": ";
-        vector<T> help = roots[i];
-        for(unsigned int j = 0; j<roots[i].size(); j++) {
+        vector<T> help = nodes[i];
+        for(unsigned int j = 0; j<nodes[i].size(); j++) {
           stream<<help[j]<<" ";
         }
         stream<<endl;
       }
     }
   
-	vector<bool> alreadycheckedroots;	}
+	vector<bool> checkednodes;
   void dfs(unsigned index, std::ostream & stream ) {
 	//неверно
-	//for (unsigned i = 0; i < size; i++) {
-	//	alreadycheckedroots.push_back(false);
-	//}
-	//неверно
-	alreadycheckedroots[index] = true;
-	stream << index << " ";
-	for (const auto& i : roots[index])
+	for (unsigned i = 0; i < size; i++) {
+		checkednodes.push_back(false);
+	}
+	checkednodes[index] = true;
+	checkednodes.push_back(index);
+	for (const auto& i : nodes[index])
 	{
-		if (!alreadycheckedroots[i])
+		if (!checkednodes[i])
 			dfs(i,stream); // dfs(i,cout)
 		}
 	}
-  ~graph() {}
+	void printdepth(std::ostream & stream) {
+		for(unsigned int i = 0; i<checkednodes.size(); i++) {
+			stream<<checkednodes[i]<<" ";
+		}
+	}
+  ~graph() {
+  	nodes.clear();
+	size = 0;  
+  }
 };
