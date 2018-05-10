2a85579e768d53aa2c89b4f6986355094c8caaed
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-05-10 18:00:30 +0300

    Update graph.cpp

--- tests/graph.cpp
+++ tests/graph.cpp
@@ -33,7 +33,7 @@ TEST_CASE("Inserting")
 {
 	graph<int> graph1({{1,2},{3},{3},{}});
 	std::ostringstream ostream;
-	graph1.insert({{1},{2},{3}});
+	graph1.insert({1,2,3});
 	graph1.print(ostream);
 	REQUIRE(ostream.str() == "0: 1 2 \n"
 				"1: 3 \n"
